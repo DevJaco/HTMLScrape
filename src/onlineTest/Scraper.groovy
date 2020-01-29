fde46113ae1da426a8fc0c09681a4310b558d5a7
DevJaco <dev2708dev@gmail.com>
2020-01-29 14:32:39 +0200

    Final Test Submission
    Due to the Eclipse IDE and the use of plugins such as Jsoup, this code was written using Groovy in a Java application. This class/script parses the given webpage and displays the list of Tech under each heading

---
+++ src/onlineTest/Scraper.groovy
@@ -0,0 +1,54 @@
+package onlineTest
+
+import groovy.json.JsonOutput
+
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import org.jsoup.select.Elements
+
+
+class HTMLScrape {
+	
+	public static void main(String[] args) {
+		def doc = Jsoup.connect("https://github.com/egis/handbook/blob/master/Tech-Stack.md").get()
+		
+		//Scraping headers
+		def block = doc.select("#readme").first()
+		def headers = block.select("h2")
+		
+		//Map in order to store the information for JSON output
+		
+		def obj = [:]
+		
+		headers.each { header -> 
+			
+			//Array in order to hold the list of technologies
+			
+			
+			def techlist = []
+			
+			//println(header.text())
+			//println()
+			
+			//A table always follows a header and therefore we can use the nextElementSibling function
+			def table = header.nextElementSibling()
+			def tableRows = table.select("tbody").select("tr")
+			
+			//Adding the tech to the array
+			tableRows.each { row ->
+				techlist.add(row.select("td").first().text())
+				//println(row.select("td").first().text())
+			
+			}
+			
+			obj.put(header.text(), techlist)
+			//println JsonOutput.prettyPrint(JsonOutput.toJson(obj))
+		}
+		
+		
+	}
+	
+	
+	}
+
+
