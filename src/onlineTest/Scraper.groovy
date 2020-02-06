fe88ea3e1105833edb0b7fbb3373c0e18bd0695c
DevJaco <dev2708dev@gmail.com>
2020-02-06 11:54:20 +0200

    Added test asserts
    I have added a getter method within the scraper class in order to access the techMap. I have also added Asserts to the test cases

--- src/onlineTest/Scraper.groovy
+++ src/onlineTest/Scraper.groovy
@@ -9,35 +9,55 @@ import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 class Scraper {
-	private Element information;
-	private Elements headers;
+	private Element information
+	private Elements headers
 	private LinkedHashMap techMap 
 	
 	public Scraper(String url) {
 		information = Jsoup.connect(url).get().select("#readme").first()
+		if(information.is(null)) {
+			throw new Exception("That webpage cannot be scraped")
+		}
 		headers = information.select("h2")
 		techMap = new LinkedHashMap<String,ArrayList>()
 		
 		headers.each { header ->
-			def techArr = []			
+			def techArr = []	
 			def tableRows = header.nextElementSibling().select("tbody").select("tr")
-			
 			
 			tableRows.each { row ->
 				techArr.add(row.select("td").first().text())
 			}
-			techMap.put(header.text(), techArr)
+			addToTechMap(header.text(), techArr);
 		}
 	}
 	
+	public void addToTechMap(String header, ArrayList tech) {
+		if(header.isEmpty()) {
+			throw new Exception("Cannot add empty header")
+		}
+		if(tech.is(null)) {
+			throw new Exception("Cannot add null array to map")
+		}
+		if(tech.isEmpty()) {
+			throw new Exception("Cannot add empty array to map")
+			
+		}
+		if(techMap.is(null)) {
+			throw new Exception("Cannot put to null object")
+		}
+		techMap.put(header, tech)
+	}
 	
-	public void outputTechMapJson() {
-		println( JsonOutput.prettyPrint(JsonOutput.toJson(techMap)) )
+	public LinkedHashMap getTechMap() {
+		return techMap;
+	}
+	public String outputTechMapJson() {
+		return JsonOutput.prettyPrint(JsonOutput.toJson(techMap)) 
 	}
 	
 	public static void main(String[] args) {
-
 		def scraper = new Scraper("https://github.com/egis/handbook/blob/master/Tech-Stack.md")
-		scraper.outputTechMapJson()
+		println(scraper.outputTechMapJson())
 	}
 }
--- src/onlineTest/ScraperTest.java
+++ src/onlineTest/ScraperTest.java
@@ -1,7 +1,9 @@
 package onlineTest;
 
+
 import java.util.ArrayList;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ScraperTest {
@@ -10,6 +12,7 @@ public class ScraperTest {
   @Test
   public void ScrapeInfoTest() {
     Scraper scraper = new Scraper("https://github.com/egis/handbook/blob/master/Tech-Stack.md");
+    Assert.assertNotNull(scraper);
   }
   
   @Test
@@ -21,6 +24,86 @@ public class ScraperTest {
 	tech.add("b");
 	
 	scraper.addToTechMap(header, tech);
+	Assert.assertEquals(scraper.getTechMap().size(), 5);
 
+  }
+  
+  @Test 
+  public void outputJsonTest() {
+	Scraper scraper = new Scraper("https://github.com/egis/handbook/blob/master/Tech-Stack.md");
+	Assert.assertEquals(scraper.outputTechMapJson(), "{\n" + 
+			"    \"Programming Stack\": [\n" + 
+			"        \"Java 8 / JVM\",\n" + 
+			"        \"Groovy\",\n" + 
+			"        \"Egis Kernel\",\n" + 
+			"        \"Hibernate / JPA\",\n" + 
+			"        \"Jetty\",\n" + 
+			"        \"Hazelcast\",\n" + 
+			"        \"PostgreSQL / MySQL / SQL Server\",\n" + 
+			"        \"Lucene\",\n" + 
+			"        \"TestNG\",\n" + 
+			"        \"C#\",\n" + 
+			"        \"Electron\",\n" + 
+			"        \"Golang\",\n" + 
+			"        \"Python\",\n" + 
+			"        \"Markdown\",\n" + 
+			"        \"Gitbook\",\n" + 
+			"        \"ES6 / Babel\",\n" + 
+			"        \"NPM\",\n" + 
+			"        \"Yarn\",\n" + 
+			"        \"Gulp\",\n" + 
+			"        \"Selenium/Webdriver\",\n" + 
+			"        \"Karma / Jasmine\",\n" + 
+			"        \"ExtJS\",\n" + 
+			"        \"Angular (1 or 2)\",\n" + 
+			"        \"Vue.js\",\n" + 
+			"        \"WebComponents / Polymer 2.0\",\n" + 
+			"        \"React\"\n" + 
+			"    ],\n" + 
+			"    \"Build Stack\": [\n" + 
+			"        \"Gradle\",\n" + 
+			"        \"Ant\",\n" + 
+			"        \"Babel / Gulp\",\n" + 
+			"        \"npm\",\n" + 
+			"        \"Github\",\n" + 
+			"        \"CircleCI\",\n" + 
+			"        \"AppVeyor\",\n" + 
+			"        \"Jenkins\",\n" + 
+			"        \"Codacy\",\n" + 
+			"        \"Codecov\",\n" + 
+			"        \"Amazon S3\",\n" + 
+			"        \"Nexus / JFrog\"\n" + 
+			"    ],\n" + 
+			"    \"Infrastructure\": [\n" + 
+			"        \"Ubuntu LTS\",\n" + 
+			"        \"Docker\",\n" + 
+			"        \"PostgreSQL\",\n" + 
+			"        \"ansible\",\n" + 
+			"        \"VMWare ESXi\",\n" + 
+			"        \"VMWare vCenter\",\n" + 
+			"        \"Amazon EC2\",\n" + 
+			"        \"Amazon S3\",\n" + 
+			"        \"Amazon DynomoDB\",\n" + 
+			"        \"Cloudflare\",\n" + 
+			"        \"Google Drive\",\n" + 
+			"        \"Sendgrid\",\n" + 
+			"        \"Google Apps\",\n" + 
+			"        \"Hipchat\",\n" + 
+			"        \"Slack\",\n" + 
+			"        \"Trello\",\n" + 
+			"        \"Fortigate\",\n" + 
+			"        \"Dell\",\n" + 
+			"        \"Docker Hub\"\n" + 
+			"    ],\n" + 
+			"    \"Monitoring\": [\n" + 
+			"        \"Dripstat\",\n" + 
+			"        \"Plumbr\",\n" + 
+			"        \"Newrelic\",\n" + 
+			"        \"Pagerduty\",\n" + 
+			"        \"Nodegrid\",\n" + 
+			"        \"health dashboard\"\n" + 
+			"    ]\n" + 
+			"}" + 
+			"");
   }
 }
